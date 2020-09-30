5b1816765cbb66a9823c270e39206b99b1d33554
Phong Tran <anhphong.tran@codestringers.com>
2020-09-30 10:56:16 +0700

    mocha config

---
+++ test.config.js
@@ -0,0 +1,14 @@
+exports.config ={
+    runner:'local',
+
+    //Derlace sever info
+    hostname: 'localhost',
+    port: 9515,
+    path:'/',
+
+    // Mocha
+    framework: 'mocha',
+    mochoOpts:{
+        ui: 'bdd'
+    }
+}
